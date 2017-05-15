50d89ffc2028e5d10832620169698d5f472a15d5
houhou1349010738 <1349010738@qq.com>
2017-05-15 20:35:02 +0800

    text1

---
+++ app/src/main/java/yipengyu/bawai/com/textfortext/JavaAndroid.java
@@ -0,0 +1,11 @@
+package yipengyu.bawai.com.textfortext;
+
+/**
+ * 项目名称：Textfortext
+ * 项目创建人：caijiannan
+ * 项目创建时间:2017/5/15 20:34
+ */
+
+public class JavaAndroid {
+    //dgsydgsdsdgshsgshdgs
+}
