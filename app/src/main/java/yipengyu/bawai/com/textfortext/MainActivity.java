40dc45268317d6e955f427e14486162872b70dfc
SYYP <m18310482720>
2017-05-15 20:28:42 +0800

    Initial commit

---
+++ app/src/main/java/yipengyu/bawai/com/textfortext/MainActivity.java
@@ -0,0 +1,18 @@
+package yipengyu.bawai.com.textfortext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        System.out.println("蔡监");
+    }
+}
